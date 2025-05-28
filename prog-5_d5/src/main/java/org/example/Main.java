8d5e6fc3a48b2b48fd4f09187ad516e0480f825b
Allan-12 <hei.allan.3@gmail.com>
2025-05-28 09:35:07 +0300

    Add Main class as entry point for coffee machine simulation

--- prog-5_d5/src/main/java/org/example/Main.java
+++ prog-5_d5/src/main/java/org/example/Main.java
@@ -1,7 +1,12 @@
 package org.example;
 
+import org.example.modele.InterfaceUtilisateur;
+import org.example.modele.MachineACafe;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        MachineACafe machine = new MachineACafe();
+        InterfaceUtilisateur ui = new InterfaceUtilisateur(machine);
+        ui.demarrer();
     }
 }
